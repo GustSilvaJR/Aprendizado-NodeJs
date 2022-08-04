2bd855dd2e1ba6f4a6fb526aef8c66bfe36dfc80
GustSilvaJR <gust.offic@gmail.com>
2022-08-04 16:49:02 -0300

    Parametros de requisições | Query params,  body params  and router params

--- Node_Js_Rocket/src/index.js
+++ Node_Js_Rocket/src/index.js
@@ -3,14 +3,20 @@ const express = require('express'); //Importando o modulo
 const app = express(); // Aqui estou usando uma função do modulo
 
 app.get("/courses", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    const query = request.query;
+    console.log(query);
     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    const body = request.body;
+    console.log(body);
     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    const params = request.params;
+    console.log(params);
     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 0"]);
 });
 
