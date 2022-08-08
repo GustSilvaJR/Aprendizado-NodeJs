71a3da638d6faa1fa32019c22b219aac09090f7d
GustSilvaJR <gust.offic@gmail.com>
2022-08-08 16:59:43 -0300

    Iniciando projeto finans | Inserindo primeira funcionalidade de salvar um cliente utilizando uma requisição do tipo post  e tratamento para impedir que se cadastre um usuário repetido

--- Node_Js_Rocket/src/index.js
+++ Node_Js_Rocket/src/index.js
@@ -1,31 +1,99 @@
 const express = require('express'); //Importando o modulo
+const {v4 : uuidv4} = require('uuid');
 
 const app = express(); // Aqui estou usando uma função do modulo
+const customers = [];
 
-app.get("/courses", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
-    const query = request.query;
-    console.log(query);
-    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+app.use(express.json()); //Utilizando um midlleware para conseguir receber Json
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body; //Utilizando desestruturação
+    const id = uuidv4();
+
+    //Utilizando a função some para iterar em meu array de objetos representando contas cadastradas para verificar se há algum cpf ja cadastrado
+    const customerAlreadyExists = customers.some( 
+        (customer) => customer.cpf == cpf
+    );
+    
+    console.log(customerAlreadyExists, cpf, customers)
+
+    if(customerAlreadyExists){
+        return response.status(400).json({error:"Customer already exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement:[]
+    });
+
+    response.status(201).send(); //Retornando status 201, que é quando um dado foi salvo
+
 });
 
-app.post("/courses", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
+app.listen(3333);
 
-app.put("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
-    const params = request.params;
-    console.log(params);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 0"]);
-});
 
-app.patch("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
-    return response.json(["Curso 1", "Curso 2", "Curso -1", "Curso 0"]);
-});
 
-app.delete("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 0"]);
-});
 
-app.listen(3333)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //app.get("/courses", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    //     const query = request.query;
+    //     console.log(query);
+    //     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+    // });
+    
+    // app.post("/courses", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    //     const body = request.body;
+    //     console.log(body);
+    //     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+    // });
+    
+    // app.put("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    //     const params = request.params;
+    //     console.log(params);
+    //     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 0"]);
+    // });
+    
+    // app.patch("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    //     return response.json(["Curso 1", "Curso 2", "Curso -1", "Curso 0"]);
+    // });
+    
+    // app.delete("/courses/:id", (request, response) => { //Criando uma rota para uma requisição get. Para url que seja localhost:3333/
+    //     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 0"]);
+    // });
