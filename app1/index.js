d4bc937e0271ff83031aa1043ef22967ea5fb63f
GustSilvaJR <gust.offic@gmail.com>
2022-08-18 20:52:04 -0300

    Arquivo responsável por representar nossa aplicação como um todo, onde definimos a porta inicial para que o node deva ficar escutando requisições

---
+++ app1/index.js
@@ -0,0 +1,12 @@
+process.title = 'MyApp'; // Caso alguém analise o processo/serviço em relação a este servidor, irá notar ler este nome
+
+const args = process.argv; //Este argv é um atributo de process que armazena as variáveis vindas via command line quando o node js é inicializado
+
+const port = args[2] || 7070 //Se tiver sido passado a porta como parametro para args, usa ela. Se não, utiliza uma default 7070.
+
+const webServer = require('./server');
+
+//Este metodo listen é responsável pela definição de qual porta o cliente irá utiliar para requisitar serviços do servidor
+webServer.listen(port, function(){
+    console.log("Server started at port " + port)
+})
