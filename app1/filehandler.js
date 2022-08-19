a7ddbc0c77da8db1e0cee1db85d1091d3dc3fe0b
GustSilvaJR <gust.offic@gmail.com>
2022-08-19 15:24:38 -0300

    Ajustes finais no template base | incluindo eslint para padronização do código seguindo as normas airbnb

--- app1/filehandler.js
+++ app1/filehandler.js
@@ -1,11 +1,11 @@
-var fs = require("fs");
+const fs = require('fs');
 
-module.exports = function(filename, successFn, errorFn){
-    fs.readFile(filename, function(err, data) {
-        if(err) {
-            errorFn(err);
-        }else{
-            successFn(data);
-        }
-    });
-}
+module.exports = function (filename, successFn, errorFn) {
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      errorFn(err);
+    } else {
+      successFn(data);
+    }
+  });
+};
--- app1/server.js
+++ app1/server.js
@@ -1,50 +1,50 @@
-//Importando modulo nativo do node js que por exemplo irá permitir que eu crie uma instancia do meu servidor
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-len */
+// Importando modulo nativo do node js que por exemplo irá permitir que eu crie uma instancia do meu servidor
 const http = require('http');
 
-const config = require('./config');
+const { parse } = require('url');
+const config = require('./config.json');
 const fileHandler = require('./filehandler');
 
-//Importando o metodo parse de um dos modulos nativos do node js
-const parse = require('url').parse;
+// Importando o metodo parse de um dos modulos nativos do node js
 
-const types = config.types;
-const rootFolder = config.rootFolder;
-const defaultIndex = config.defaultIndex;
-var server;
+const { types } = config;
+const { rootFolder } = config;
+const { defaultIndex } = config;
 
-//Exportando para uso dos demais arquivos da aplicação uma instancia do meu servidor, que a partir do http.createServer() é criada
-module.exports = server = http.createServer();
+// Exportando para uso dos demais arquivos da aplicação uma instancia do meu servidor, que a partir do http.createServer() é criada
+const server = http.createServer();
+module.exports = server;
 
-//A partir dessa instancia, consigo criar eventos e definir o que vai ocorrer de acordo com os parametros passados
-//Nesse caso, quando o servidor receber um request, deverá executar a função onRequest. O evento request, é predefinido pelo servidor http do nodejs
+// A partir dessa instancia, consigo criar eventos e definir o que vai ocorrer de acordo com os parametros passados
+// Nesse caso, quando o servidor receber um request, deverá executar a função onRequest. O evento request, é predefinido pelo servidor http do nodejs
 server.on('request', onRequest);
 
- function onRequest(req, res) {
-  //A partir do metodo parse, consigo extrair um json com partes estrategicas da minha url, como pathname que é o arquvio requisitado, host, port, search e outros
-  var filename = parse(req.url).pathname,
-      fullPath,
-      extension;
+function onRequest(req, res) {
+  // A partir do metodo parse, consigo extrair um json com partes estrategicas da minha url, como pathname que é o arquvio requisitado, host, port, search e outros
+  let filename = parse(req.url).pathname;
 
-  if(filename === '/') {
-      filename = defaultIndex;
+  if (filename === '/') {
+    filename = defaultIndex;
   }
 
-  //Atribuo o caminho total até o arquivo que o usuario deseja acessar, sendo rootFolder vindo mdo meu arquivo config e representando minha pasta raiz e filename o nome do aruqivo que se deseja acessar
-  fullPath = rootFolder + filename;
+  // Atribuo o caminho total até o arquivo que o usuario deseja acessar, sendo rootFolder vindo mdo meu arquivo config e representando minha pasta raiz e filename o nome do aruqivo que se deseja acessar
+  const fullPath = rootFolder + filename;
 
-  //Retorno a extensão do filename que o usuário pretende acessar, para descobrir o tipo do arquivo que será apresentado pelo browser
-  let split = filename.split(".");
-  extension = split[split.length - 1];
+  // Retorno a extensão do filename que o usuário pretende acessar, para descobrir o tipo do arquivo que será apresentado pelo browser
+  const split = filename.split('.');
+  const extension = split[split.length - 1];
 
-  fileHandler(fullPath, function(data) {
-      res.writeHead(200, {
-           'Content-Type': types[extension] || 'text/plain',
-           'Content-Length': data.length
-      });
-      res.end(data);
-
-  }, function(err) {
-      res.writeHead(404);
-      res.end();
+  fileHandler(fullPath, (data) => {
+    res.writeHead(200, {
+      'Content-Type': types[extension] || 'text/plain',
+      'Content-Length': data.length,
+    });
+    res.end(data);
+  // eslint-disable-next-line no-unused-vars
+  }, (err) => {
+    res.writeHead(404);
+    res.end();
   });
- }
+}
