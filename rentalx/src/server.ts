b0b9f27a9f01783d7ae71ccc00c6a8c0596510bf
GustSilvaJR <gust.offic@gmail.com>
2022-08-31 11:47:54 -0300

    Ajustando arquivo para início do projeto Rentalx

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -2,14 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response)=>{
-  return response.json('Hello World! Welcome Sr.');
-});
-
-app.post('/courses', (request, response)=>{
-  const { body } = request.body;
-
-  return response.json(body);
-});
+app.use(express.json());
 
 app.listen(3333);
