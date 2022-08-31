ff259bb20a89c0f2c508bc70191b645e0e528a4b
Gustavo Alessandro <gust.offic@gmail.com>
2022-08-31 08:18:32 -0300

    Adicionando rota post para efetuar  testes usando debug validando variaveis passadas via corpo da requisição

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -3,7 +3,13 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response)=>{
-  return response.json('Hello World');
+  return response.json('Hello World! Welcome Sr.');
+});
+
+app.post('/courses', (request, response)=>{
+  const { body } = request.body;
+
+  return response.json(body);
 });
 
 app.listen(3333);
