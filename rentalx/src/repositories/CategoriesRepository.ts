32fe70659fb2af48f2a21d64efe8bf6fbaee33f0
GustSilvaJR <gust.offic@gmail.com>
2022-09-01 15:36:00 -0300

    Criando meu repository que servirá como minha conexao com o banco para persistencia de dados e demais consulta a respeito da categoria. Nesse momento, utilizando um array do tipo Category para exemplo | Criação no método create para persistir uma categoria dentro do array simulando o banco | Criação de uma interface  para utilizarmos DTO no recebimento dos dados vindo da rota

---
+++ rentalx/src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from '../models/Category';
+
+//Criando interface que irá representar meu DTO(DATA TRANSFER OBJECT), que servirá para transportar os dados da minha rota para o meu repository
+//Utilizando deste recurso, é possível receber o objeto necessário, sem que o arquivo de rota tenha a necessidade de importar o model Category
+//para criar o objeto que será enviado pelo parâmetro. Isso é uma boa pática
+interface ICategoryDTO {
+  name: string,
+  description: string,
+}
+
+export class CategoryRepository {
+
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICategoryDTO): void {
+
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+  }
+
+}
