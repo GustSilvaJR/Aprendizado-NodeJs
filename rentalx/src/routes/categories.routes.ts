fe5cf0abea51b5072f8fa0ee8a44e4382dcde027
GustSilvaJR <gust.offic@gmail.com>
2022-09-01 16:30:18 -0300

    Criando rota getCategories para receber o request das caregorias e a partir da função list criada em CategoriesRepository, retornar o array de objetos do tipo Category

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -2,15 +2,22 @@ import { Router } from 'express';
 import { CategoryRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRouter = Router();
+const categoryRepository = new CategoryRepository();
 
 categoriesRouter.post('/', (request, response)=>{
 
   const { name, description } = request.body;
 
-  const categoryRepository = new CategoryRepository();
   categoryRepository.create({ name, description });
 
-  return response.status(201);
+  return response.status(201).send();
+});
+
+categoriesRouter.get('/', (request, response) =>{
+
+  let list = categoryRepository.listar();
+
+  return response.status(200).json({ 'Categories':list });
 });
 
 export { categoriesRouter };
