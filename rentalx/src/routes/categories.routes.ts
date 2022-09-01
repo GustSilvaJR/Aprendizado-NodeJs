955ee53567ea006cf6cdb60bee43b4a0bfa7ee97
GustSilvaJR <gust.offic@gmail.com>
2022-09-01 14:26:04 -0300

    Ajustando rota para criaçãod e categorias segundo o encapsulamento feito na classe Category

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,22 +1,25 @@
 import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
+import { Category } from '../models/Category';
 
 const categoriesRouter = Router();
 
-const categories = [];
+const categories: Category[] = []; //Criei um array chamado categories, que é do tipo Category
 
 categoriesRouter.post('/', (request, response)=>{
 
   const { name, description } = request.body;
 
-  categories.push({
-    id: uuid(),
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
 
-  return response.status(201).json({ 'objects': categories });
+  categories.push(category);
 
+  return response.status(201).json({ 'object': category });
 });
 
 export { categoriesRouter };
