170c3a40d589bb477cf37b346d7ea9af5646eeaa
Gustavo Alessandro <gust.offic@gmail.com>
2022-09-19 13:47:47 -0300

    Criando interface que represente minha CategoryRepository

---
+++ rentalx/src/repositories/ICateogoryRepository.ts
@@ -0,0 +1,12 @@
+import { Category } from '../models/Category';
+
+interface ICategoryDTO {
+  name: string,
+  description: string,
+}
+
+interface ICategoryRepository {
+  findAlreadyExists(name: string) : boolean;
+  listar():Category[];
+  create({ name, description }: ICategoryDTO): void;
+}
