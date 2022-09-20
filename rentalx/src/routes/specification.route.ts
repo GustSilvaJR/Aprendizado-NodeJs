7f23763338ef28bf46ef2adb959fd8043cfac1b1
GustSilvaJR <gust.offic@gmail.com>
2022-09-20 08:09:32 -0300

    Ajustando padrão de nomeclatura de variavel

--- rentalx/src/routes/specification.route.ts
+++ rentalx/src/routes/specification.route.ts
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import { SpecificationRepository } from '../modules/cars/repositories/SpecificationRepository';
 import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
 
-const specifications = Router();
+const specificationsRouter = Router();
 const specificationRepository = new SpecificationRepository;
 
-specifications.post('/', (request, response)=>{
+specificationsRouter.post('/', (request, response)=>{
   const { name, description } = request.body;
   const createServiceSpecification = new CreateSpecificationService(specificationRepository);
 
   createServiceSpecification.execute({ name, description });
   return response.status(201).send();
 });
+
+export { specificationsRouter };
