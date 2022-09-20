ff279a3ddb1f78a699bcd3a1fa752b189b0ec808
GustSilvaJR <gust.offic@gmail.com>
2022-09-20 14:39:31 -0300

    Utilizando e definindo o caminho para a rota index

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { categoriesRouter } from './routes/categories.routes';
+import { index } from './routes/index.routes';
 import { specificationsRouter } from './routes/specification.route';
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/categories', categoriesRouter);
 app.use('/specifications', specificationsRouter);
+app.use('/home', index);
 
 app.listen(3333);
