c8caed027c5dd65345394225bbb3f8e78f676201
GustSilvaJR <gust.offic@gmail.com>
2022-09-29 16:53:50 -0300

    Implementação da rota import, para importar arquivos

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { categoryCreateController } from '../modules/cars/useCases/category/createCategory';
 import { categoryListController } from '../modules/cars/useCases/category/listCategory';
-
+import { categoryImportController } from '../modules/cars/useCases/category/importCategory';
 
 const categoriesRouter = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRouter.post('/', (request, response)=>{
   categoryCreateController.handle(request, response);
@@ -12,6 +17,10 @@ categoriesRouter.post('/', (request, response)=>{
 
 categoriesRouter.get('/', (request, response) =>{
   categoryListController.handle(request, response);
+});
+
+categoriesRouter.post('/import', upload.single('file'), (request, response)=>{
+  categoryImportController.handle(request, response);
 });
 
 export { categoriesRouter };
