203eb9f62d919249a727e2f6a58af06418c28631
GustSilvaJR <gust.offic@gmail.com>
2022-09-29 14:01:47 -0300

    Criando diretório para separar as implementações das minhas interfaces

---
+++ rentalx/src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,53 @@
+import { Category } from '../../models/Category';
+import { ICategoryRepository } from '../ICateogoryRepository';
+
+//Criando interface que irá representar meu DTO(DATA TRANSFER OBJECT), que servirá para transportar os dados da minha rota para o meu repository
+//Utilizando deste recurso, é possível receber o objeto necessário, sem que o arquivo de rota tenha a necessidade de importar o model Category
+//para criar o objeto que será enviado pelo parâmetro. Isso é uma boa pática
+
+interface ICategoryDTO {
+  name: string,
+  description: string,
+}
+
+export class CategoriesRepository implements ICategoryRepository {
+
+  private static _INSTANCE: CategoriesRepository;
+  private categories: Category[];
+
+  private constructor() {
+    this.categories = [];
+  }
+
+  public static getInstance():CategoriesRepository{
+    if(!CategoriesRepository._INSTANCE){
+      CategoriesRepository._INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository._INSTANCE;
+  }
+
+  create({ name, description }: ICategoryDTO): void {
+
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+  }
+
+  listar(): Category[] {
+    return this.categories;
+  }
+
+  findAlreadyExists(name: string): boolean {
+    let exists = this.categories.some((category) => category.name === name);
+
+    return exists;
+  }
+
+}
---
+++ rentalx/src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -0,0 +1,42 @@
+import { Specification } from '../../models/Specification';
+import { ISpecificationRepository, ISpecificationDTO } from '../ISpecificationRepository';
+
+class SpecificationRepository implements ISpecificationRepository {
+
+  private static _INSTANCE:SpecificationRepository;
+  private _specifications: Specification[];
+
+  private constructor() {
+    this._specifications = [];
+  }
+
+  public static getInstance():SpecificationRepository{
+    if(!SpecificationRepository._INSTANCE){
+      SpecificationRepository._INSTANCE = new SpecificationRepository();
+    }
+    return SpecificationRepository._INSTANCE;
+  }
+
+  create({ name, description }: ISpecificationDTO): void {
+    const specification = new Specification();
+    
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this._specifications.push(specification);
+  }
+
+  list():Specification[] {
+    return this._specifications;
+  }
+
+  findByName(name: string): Specification {
+    const specificationAlreadyExists = this._specifications.find(specification => (specification.name === name));
+    return specificationAlreadyExists;
+  }
+}
+
+export { SpecificationRepository };
