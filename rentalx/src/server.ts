1dff5d9564034e4727d3a4ad2a878a7caee8e41a
GustSilvaJR <gust.offic@gmail.com>
2022-10-04 14:09:33 -0300

    Criando rota para meu arquivo de documentações utilizando o swagger

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import swagerUi from 'swagger-ui-express';
+import swagerFile from './swager.json';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swagerUi.serve, swagerUi.setup(swagerFile));
+
 app.use(router);
 
 app.listen(3333);
