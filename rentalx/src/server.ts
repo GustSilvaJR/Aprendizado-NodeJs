530ea790947be61b752aaa86e7524fd0928ce74b
GustSilvaJR <gust.offic@gmail.com>
2022-10-06 09:23:50 -0300

    Criando rota para meus arquivos estáticos

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use('/api-docs', swagerUi.serve, swagerUi.setup(swagerFile));
 
+app.use('/static', express.static(__dirname + '/public'));
+
 app.use(router);
 
 app.listen(3333);
