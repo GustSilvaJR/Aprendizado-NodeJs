2d155710761775bb8d982dfbe57340f2b0bddda6
GustSilvaJR <gust.offic@gmail.com>
2022-10-18 14:14:58 -0300

    Chamando função inicializaConexao, responsavel por inicializar minha conexao dataSource e printar se a conexao deu certo ou nao, retornando true ou false respectivamente

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -2,11 +2,10 @@ import express from 'express';
 import swagerUi from 'swagger-ui-express';
 import swagerFile from './swager.json';
 import { router } from './routes';
-import { PostgresDataSource } from './database';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { inicializaConexao, PostgresDataSource } from './database';
 
 const app = express();
-
-console.log(PostgresDataSource.isInitialized);
 
 app.use(express.json());
 
@@ -15,5 +14,7 @@ app.use('/api-docs', swagerUi.serve, swagerUi.setup(swagerFile));
 app.use('/static', express.static(__dirname + '/public'));
 
 app.use(router);
+
+inicializaConexao();
 
 app.listen(3333);
