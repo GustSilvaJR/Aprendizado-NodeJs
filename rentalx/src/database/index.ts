d2abef12e15d16382aed84c8929c2f149f67ea9e
GustSilvaJR <gust.offic@gmail.com>
2022-10-18 14:19:41 -0300

    Implementando meu dataSource e corrigindo alguns detalhes anteriores a respeito do banco | Implementando uma função que sera responsavel por inicializar minha conexao com o banco e printar ao fim um validador se foi criada com sucesso ou nao

--- rentalx/src/database/index.ts
+++ rentalx/src/database/index.ts
@@ -1,20 +1,32 @@
 import { DataSource } from 'typeorm';
 
+//using environment variables
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const PostgresDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: 'database_ignite',
   port: 5432,
   username: 'docker',
-  password: 'docker',
+  password: 'ignite',
   database: 'rentalx',
 });
 
-PostgresDataSource.initialize()
-  .then(()=>{
-    console.log('Data Source Initialized');
-  })
-  .catch((error)=>{
-    console.error('Error During Initializing Data Source: ' + error);
-  });
+async function  inicializaConexao() {
+  await PostgresDataSource.initialize()
+    .then(() => {
+      console.log('Data Source Initialized');
+      console.log(PostgresDataSource.isInitialized);
+    })
+    .catch((error) => {
+      console.error('Error During Initializing Data Source: ' + error);
+    });
+}
 
-export { PostgresDataSource };
+
+
+
+
+export { PostgresDataSource, inicializaConexao };
