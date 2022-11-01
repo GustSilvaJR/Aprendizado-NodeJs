150087a38abb3dc0afaab828c7bfb4a3ed925da7
GustSilvaJR <gust.offic@gmail.com>
2022-11-01 10:27:49 -0300

    Inserindo retorno de uma tabela com os dados consultados pelo usuario em um input text, que é acionado quando se clica em um botao

--- node-react/client/src/App.js
+++ node-react/client/src/App.js
@@ -1,22 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {
-    response: ''
+    response: [],
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
+
+  renderRow(row) {
+    return (
+      <tr>
+        <td>{row.id}</td>
+        <td>{row.numero}</td>
+      </tr>
+    )
+  }
+
+  getResponse(url) {
+    let cod = document.querySelector("#codigoFilial").value;
+    this.callApi(url, cod)
+      .then(res => {
+        this.setState({ response: res.express });
+        console.log(this.state.response);
+      })
       .catch(err => console.log(err));
   }
 
-  callApi = async () => {
-    const response = await fetch('');
+  callApi = async (url, data = '0') => {
+
+    url += '?' + new URLSearchParams({ codigoFilial: data });
+    console.log(url);
+
+    const response = await fetch(url);
+
+    if (response.status !== 200) throw Error();
+
     const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
+    console.log(body);
+    console.log(body.express);
 
     return body;
   };
@@ -28,7 +50,26 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        <div className='row center mt-3 mb-1'>
+          <label className='col-1' htmlFor="codigoFilial">Código Filial</label>
+          <input className='col-3 m-2' id="codigoFilial" type='text' placeholder='Filial a se consultar' />
+          <button className='btn btn-primary col-2' onClick={() => { this.getResponse('/instrucaoServicoReal') }}>Click and Consult</button>
+        </div>
+
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Número</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.response.map(this.renderRow)}
+          </tbody>
+        </table>
+
       </div>
     );
   }
