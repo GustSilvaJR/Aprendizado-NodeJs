fc72c091649282c43f5fa71258a1eedbbc262b07
GustSilvaJR <gust.offic@gmail.com>
2022-11-16 16:46:37 -0300

    Criando arquivo com estilos globais da aplicação. Utilizando o styled components para criação do estilo

--- ControleVendasMarmitex/myapp/src/css/app.ts
+++ ControleVendasMarmitex/myapp/src/css/app.ts
@@ -1,10 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
-*{
-    margin: 0px;
-    padding: 0px;
-}
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0px;
+        padding: 0px;
+    }
 
-html{
-    background-image: url('./../assets/background_app/background.jpg') repeat;
-}
+    html{
+        overflow-x:hidden;
+    }
+
+    body{
+        background-image: linear-gradient(to right, #4749b1, #455fc4, #4474d7, #4589e7, #4a9ef7);
+    }
+`
