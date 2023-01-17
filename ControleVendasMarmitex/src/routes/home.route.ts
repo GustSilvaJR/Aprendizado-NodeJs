75f03225f6b23b1e34da95667e1c30dc33b772d2
GustSilvaJR <gust.offic@gmail.com>
2023-01-17 15:38:42 -0300

    Criando rota que irá retornar todas os usuários cadastrados no banco de dados

--- ControleVendasMarmitex/src/routes/home.route.ts
+++ ControleVendasMarmitex/src/routes/home.route.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import { HomeController } from './../controllers/HomeController';
 
+
+import { AppDataSource } from '../database';
+import { User } from '../models/user.entity';
+
 const homeRoute = Router();
 const homeController = new HomeController();
 
 homeRoute.get('/api/hello', (request, response)=>{
   homeController.getApresentacao(request, response);
+});
+
+homeRoute.get('/api/getAllUsers', async (request, response)=>{
+  const users = await AppDataSource.getRepository(User).find();
+  console.log(users);
+  return response.json(users);
 });
 
 export { homeRoute };
