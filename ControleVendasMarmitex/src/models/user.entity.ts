eb56697ed4442e2cf93de9ed17a33402dfc60889
GustSilvaJR <gust.offic@gmail.com>
2023-01-17 15:27:30 -0300

    Criando entidade Usuario com as diretivas do typeorm

---
+++ ControleVendasMarmitex/src/models/user.entity.ts
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name:'usuarios',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+    id_usuario?: number;
+
+  @Column()
+    nome?: string;
+
+  @Column()
+    email?: string;
+
+  @Column()
+    senha?: string;  
+
+  @Column()
+    administrador?: boolean;  
+}
