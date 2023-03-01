c7ea1b7973a8ed2fc5f19e51a8e8eded5c075001
GustSilvaJR <gust.offic@gmail.com>
2023-03-01 16:01:05 -0300

    Finalizando retorno de dados ao efetuar o login, consumindo todos os useCases criados para retornar dados sobre a empresa, sistema e licença | Atualizando local de retorno do login, trocando responsabilidade do arquvivo de rota para o controller do login

--- ControleVendasMarmitex/src/routes/auth.route.ts
+++ ControleVendasMarmitex/src/routes/auth.route.ts
@@ -1,30 +1,13 @@
 import { Router } from 'express';
 
 //Importando controller para auth
-import { adressApiController } from '../modules/auth/useCases/adressApi';
 import { forgotPassController } from '../modules/auth/useCases/forgotPass';
 import { signInController } from '../modules/auth/useCases/login';
-import { qtdLicensesController } from '../modules/auth/useCases/getQtdLicenses';
 
 const authRoute = Router();
 
 authRoute.post('/login', async (Request, Response) => {
-  const dataResponse = await signInController.handle(Request);
-  const dataEnterprise = await adressApiController.handle(dataResponse.han_empresa);
-  const dataLicense = await qtdLicensesController.handle(dataResponse.han_empresa, dataResponse.han_system);
-
-  console.log('\n\nDataEnterprise: ', dataEnterprise);
-
-  const result = {
-    ...dataResponse,
-    ...dataEnterprise,
-    qtd_licenses: dataLicense,
-  };
-
-  console.log('\n\n ', result);
-
-  return Response.json(result);
-
+  await signInController.handle(Request, Response);
 });
 
 authRoute.post('/change-pass', (Request, Response) => {
